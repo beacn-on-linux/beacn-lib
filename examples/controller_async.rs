e2116c74e9ccc7657ccf91ddb971eef03e2c5b5e
Craig McLure <craig@mclure.net>
2026-08-02 00:09:59 +0100

    Adjusted for new async code

--- examples/controller_async.rs
+++ examples/controller_async.rs
@@ -88,11 +88,11 @@ async fn main() {
                 for device in &device_maps {
                     let (x, y, image) = test_pattern(step);
                     for (button, colour) in test_buttons(step) {
-                        let _ = device.device.set_button_colour(button, colour);
+                        let _ = device.device.set_button_colour(button, colour).await;
                     }
 
-                    let _ = device.device.send_keepalive();
-                    let _ = device.device.set_image(x, y, &image);
+                    let _ = device.device.send_keepalive().await;
+                    let _ = device.device.set_image(x, y, &image).await;
 
                     step += 1;
 
@@ -117,7 +117,7 @@ async fn main() {
     }
 
     for device in device_maps {
-        let _ = device.device.set_enabled(false);
+        let _ = device.device.set_enabled(false).await;
     }
 }
 
