a00df63908e92565cf2fdd3de9a71fb913dc2844
Craig McLure <craig@mclure.net>
2026-08-03 23:29:45 +0100

    Introduced 'async' feature which provides await without tokio / smol dependencies

--- src/setup.rs
+++ src/setup.rs
@@ -5,12 +5,12 @@ use log::info;
 use nusb::{Device, DeviceInfo, Interface};
 
 pub(crate) async fn list_devices() -> Result<impl Iterator<Item = DeviceInfo>, nusb::Error> {
-    #[cfg(any(feature = "tokio", feature = "smol", target_arch = "wasm32"))]
+    #[cfg(any(feature = "async", target_arch = "wasm32"))]
     {
         info!("Hi There");
         nusb::list_devices().await
     }
-    #[cfg(not(any(feature = "tokio", feature = "smol", target_arch = "wasm32")))]
+    #[cfg(not(any(feature = "async", target_arch = "wasm32")))]
     {
         use nusb::MaybeFuture;
         nusb::list_devices().wait()
@@ -18,11 +18,11 @@ pub(crate) async fn list_devices() -> Result<impl Iterator<Item = DeviceInfo>, n
 }
 
 pub(crate) async fn open(info: &DeviceInfo) -> Result<Device, nusb::Error> {
-    #[cfg(any(feature = "tokio", feature = "smol", target_arch = "wasm32"))]
+    #[cfg(any(feature = "async", target_arch = "wasm32"))]
     {
         info.open().await
     }
-    #[cfg(not(any(feature = "tokio", feature = "smol", target_arch = "wasm32")))]
+    #[cfg(not(any(feature = "async", target_arch = "wasm32")))]
     {
         use nusb::MaybeFuture;
         info.open().wait()
@@ -33,11 +33,11 @@ pub(crate) async fn claim_interface(
     device: &Device,
     interface: u8,
 ) -> Result<Interface, nusb::Error> {
-    #[cfg(any(feature = "tokio", feature = "smol", target_arch = "wasm32"))]
+    #[cfg(any(feature = "async", target_arch = "wasm32"))]
     {
         device.claim_interface(interface).await
     }
-    #[cfg(not(any(feature = "tokio", feature = "smol", target_arch = "wasm32")))]
+    #[cfg(not(any(feature = "async", target_arch = "wasm32")))]
     {
         use nusb::MaybeFuture;
         device.claim_interface(interface).wait()
@@ -45,11 +45,11 @@ pub(crate) async fn claim_interface(
 }
 
 pub(crate) async fn set_alt_setting(interface: &Interface, alt: u8) -> Result<(), nusb::Error> {
-    #[cfg(any(feature = "tokio", feature = "smol", target_arch = "wasm32"))]
+    #[cfg(any(feature = "async", target_arch = "wasm32"))]
     {
         interface.set_alt_setting(alt).await
     }
-    #[cfg(not(any(feature = "tokio", feature = "smol", target_arch = "wasm32")))]
+    #[cfg(not(any(feature = "async", target_arch = "wasm32")))]
     {
         use nusb::MaybeFuture;
         interface.set_alt_setting(alt).wait()
