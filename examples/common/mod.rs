4ccd0905f1cf68aa0a209f873c61280d6eefb9ad
Craig McLure <craig@mclure.net>
2026-08-04 00:29:37 +0100

    Added wasm spawn_local support, and cross platform ticker

--- examples/common/mod.rs
+++ examples/common/mod.rs
@@ -3,8 +3,11 @@ use std::time::Duration;
 pub mod controller;
 pub mod logging;
 
+// Clippy doesn't understand that there's more than one example, and flags these unused :D
+
 /// This is a runtime agnostic sleep function, it'll use tokio if inside a tokio runtime, otherwise
 /// it'll fall back to asyncio
+#[allow(unused)]
 pub(crate) async fn sleep(duration: Duration) {
     #[cfg(all(feature = "tokio", not(target_arch = "wasm32")))]
     {
@@ -25,7 +28,6 @@ pub(crate) async fn sleep(duration: Duration) {
     }
 }
 
-
 // This is kinda silly, but I don't wanna make a proc macro for this, and it's here to ensure
 // that wasm environments are built correctly without time.
 #[macro_export]
@@ -42,28 +44,87 @@ macro_rules! beacn_main {
 
     (flavor = "current_thread", $body:block) => {
         fn main() {
-            let mut runtime = tokio::runtime::Builder::new_current_thread();
+            #[cfg(target_arch = "wasm32")]
+            {
+                wasm_bindgen_futures::spawn_local(async move $body);
+            }
 
             #[cfg(not(target_arch = "wasm32"))]
-            let runtime = runtime.enable_all();
-            let runtime = runtime.build().unwrap();
-            runtime.block_on(async $body);
+            {
+                let mut runtime = tokio::runtime::Builder::new_current_thread();
+
+                let runtime = runtime.enable_all();
+                let runtime = runtime.build().unwrap();
+
+                runtime.block_on(async $body);
+            }
         }
     };
 
     (flavor = "local", $body:block) => {
         fn main() {
-            let mut runtime = tokio::runtime::Builder::new_current_thread();
+            #[cfg(target_arch = "wasm32")]
+            {
+                wasm_bindgen_futures::spawn_local(async move $body);
+            }
 
             #[cfg(not(target_arch = "wasm32"))]
-            let runtime = runtime.enable_all();
-            let runtime = runtime.build().unwrap();
-            let local = tokio::task::LocalSet::new();
-            local.block_on(&runtime, async $body);
+            {
+                let mut runtime = tokio::runtime::Builder::new_current_thread();
+
+                let runtime = runtime.enable_all();
+                let runtime = runtime.build().unwrap();
+
+                let local = tokio::task::LocalSet::new();
+                local.block_on(&runtime, async $body);
+            }
         }
     };
 
     ($body:block) => {
         $crate::beacn_main!(flavor = "current_thread", $body);
     };
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+#[allow(unused)]
+pub fn spawn_local<F>(future: F) -> tokio::task::JoinHandle<F::Output>
+where
+    F: Future + 'static,
+{
+    tokio::task::spawn_local(future)
+}
+
+#[cfg(target_arch = "wasm32")]
+#[allow(unused)]
+pub fn spawn_local<F>(future: F)
+where
+    F: Future<Output = ()> + 'static,
+{
+    wasm_bindgen_futures::spawn_local(future)
+}
+
+#[allow(dead_code)]
+pub mod interval {
+    use futures_timer::Delay;
+    use web_time::Duration;
+
+    pub struct Interval {
+        period: Duration,
+        delay: Delay,
+    }
+
+    impl Interval {
+        pub fn new(period: Duration) -> Self {
+            Self {
+                period,
+                delay: Delay::new(period),
+            }
+        }
+
+        pub async fn tick(&mut self) {
+            (&mut self.delay).await;
+            self.delay = Delay::new(self.period);
+        }
+    }
 }
